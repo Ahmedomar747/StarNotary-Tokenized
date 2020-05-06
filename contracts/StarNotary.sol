b79a72ece40128cf6dd7ec2697d893be54e4da8e
Ahmedomar747 <42421885+Ahmedomar747@users.noreply.github.com>
2020-05-06 22:56:19 +0200

    Add files via upload

--- contracts/StarNotary.sol
+++ contracts/StarNotary.sol
@@ -8,15 +8,15 @@ contract StarNotary is ERC721 {
 
     // Star data
     struct Star {
-        string name;
-        string symbol;
-        
+        string name;       
     }
 
     // Implement Task 1 Add a name and symbol properties
     // name: Is a short name to your token
     // symbol: Is a short string like 'USD' -> 'American Dollar'
     
+    string public constant name  = "StarToken";
+    string public constant symbol = "STR";
 
     // mapping the Star with the Owner Address
     mapping(uint256 => Star) public tokenIdToStarInfo;
@@ -26,7 +26,7 @@ contract StarNotary is ERC721 {
     
     // Create Star using the Struct
     function createStar(string memory _name, uint256 _tokenId) public { // Passing the name and tokenId as a parameters
-        Star memory newStar = Star(_name,'STR'); // Star is an struct so we are creating a new Star
+        Star memory newStar = Star(_name); // Star is an struct so we are creating a new Star
         tokenIdToStarInfo[_tokenId] = newStar; // Creating in memory the Star -> tokenId mapping
         _mint(msg.sender, _tokenId); // _mint assign the the star with _tokenId to the sender address (ownership)
     }
